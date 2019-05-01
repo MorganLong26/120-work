e393945080c1af1652c76cb793d3bc83b11f67d2
Morgan Long <mraelong26@gmail.com>
2019-04-30 20:58:12 -0600

    snow

--- Final/sketch.js
+++ Final/sketch.js
@@ -1,28 +1,19 @@
 // Declaring my variables
 var angle = 0;
-var slider;
-var snow = [];
+
 
 // set up the canvas
 function setup() {
 createCanvas( 500 , 450 );
 //Slider for the tree
 slider = createSlider(0, TWO_PI, PI/4, 0.01);
-//Snow Clouds
-for( var  i = 0; i < 10; i++) {
-  var x= 100 + 100 * i;
-snow.push(new Snowflake(x,0,300));
-}
+
 }
 //Draw Function
 function draw() {
   //Background Color
 background('black');
-// Object set up for the snow clounds
-for( var  i = 0; i < Snowflake.length; i++) {
-  snow[i].move();
-  snow[i].show();
-}
+
 //Slider and Branch of tree set up
 angle = slider.value();
 var len = 100;
@@ -45,16 +36,5 @@ function branch(len){
   pop();
 }
   }
-  //Class for the clouds
-  class Snowflake{
-constructor(x,y,r) {
-            this.x = x;
-            this.y = y;
-            this.r = r;
-        }
-  //Movement for the clouds
-move() {
-        this.x = this.x + random(-1, 1);
-        this.y = this.y + random(-1, 1);
-    }
+
   }
